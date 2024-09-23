f4bb42f87afd0ec5a583e05ea6d5b59eb4dc0b00
unknown <matteus.vinasco@stud.sti.se>
2024-09-23 20:52:18 +0200

    Commit 5 av Matteus: Lägger till autowired-injektion av FilmRepository för databasoperationer.

--- src/main/java/com/Siyar/ws_Projektarbete/Services/ExternalFilmService.java
+++ src/main/java/com/Siyar/ws_Projektarbete/Services/ExternalFilmService.java
@@ -15,6 +15,10 @@ import java.util.Random;
 @Service
 public class ExternalFilmService {
     private final RestTemplate restTemplate = new RestTemplate();
-
+//Lägger till autowired-injektion av FilmRepository för att hantera
+// databasoperationer relaterade till filmer i applikationen. @Autowired
+// ser till att Spring automatiskt injicerar rätt instans av FilmRepository.
+    @Autowired
+    private FilmRepository filmRepository;
 
 }
